c22fcf4e3f73360de86a392880c13814c141df8f
imaxisXD <sunny735084@gmail.com>
2023-05-19 04:49:05 +0530

    chore: code ,helper functions, cleanUps

--- resources/script.js
+++ resources/script.js
@@ -17,7 +17,8 @@ const makeTwoPartCallback = () => {
                     channel: result?.richSnippet?.person.name,
                     thumbnailImage: result?.thumbnailImage?.url,
                     url: result?.richSnippet?.videoobject?.url,
-                    views: result?.richSnippet?.videoobject?.interactioncount
+                    views: result?.richSnippet?.videoobject?.interactioncount,
+                    imageUrl: result?.metatags?.ogImage
                 })
             }
             else {
@@ -30,6 +31,7 @@ const makeTwoPartCallback = () => {
     const renderedCallback = (name, q, promos, results) => {
         for (let i = 0; i < results.length; i++) {
             const div = results[i];
+            div.addEventListener('click', handleClick);
             const parent = div.parentNode;
             parent.classList.add('parent-container');
             const genre = musicCategoryVideoes[i]["genre"];
@@ -39,7 +41,6 @@ const makeTwoPartCallback = () => {
                 div.classList.add('result-card');
 
                 const imageDiv = document.createElement('div');
-
                 const imageElement = document.createElement('img');
                 imageElement.src = musicCategoryVideoes[i]["thumbnailImage"];
                 imageElement.width = '132';
@@ -61,72 +62,28 @@ const makeTwoPartCallback = () => {
                 for (const child of childElementArray2) {
                     urlAndViewDiv.appendChild(child)
                 }
-
                 const childElementArray1 = [
                     document.createElement('p'),
                     document.createElement('p'),
                     urlAndViewDiv
                 ];
-
                 childElementArray1[0].textContent = stringLengthShortner(musicCategoryVideoes[i].title, 40);
                 childElementArray1[0].classList.add('result-card__title');
                 childElementArray1[1].textContent = musicCategoryVideoes[i].channel;
                 childElementArray1[1].classList.add('result-card__subtitle');
-
                 for (const child of childElementArray1) {
                     infoDiv.appendChild(child);
                 }
-
+                div.setAttribute('data-url', musicCategoryVideoes[i].url);
+                div.setAttribute('data-img', musicCategoryVideoes[i].imageUrl);
                 div.appendChild(infoDiv);
             }
             else {
                 parent.removeChild(div);
             }
         }
+        navBar();
         searchOnGoogleBtn();
-
-
-
-
-        navigationElement = document.getElementsByClassName('gsc-cursor');
-        pageNumberComponent = document.getElementsByClassName('gsc-cursor-page');
-        const pageArray = Array.from(pageNumberComponent);
-        const currentPageIndex = Array.from(pageArray).findIndex(div => div.classList.contains('gsc-cursor-current-page'));
-
-        const previousDiv = pageArray[currentPageIndex - 1];
-        const currentDiv = pageArray[currentPageIndex];
-        const nextDiv = pageArray[currentPageIndex + 1];
-
-        previousDiv.classList.add('navBtn')
-        nextDiv.classList.add('navBtn')
-        currentDiv.classList.add('curr-navBtn')
-
-        // Custom Navigation Bar
-        if (navigationElement[0]) {
-            //Left Arrow            
-            const leftArrowDiv = document.createElement('div');
-            const leftArrowImage = document.createElement('img');
-            previousDiv.innerText = 'Prev'
-            leftArrowImage.src = './resources/Images/leftArrow.svg';
-            leftArrowImage.width = '16';
-            leftArrowImage.height = '16';
-            leftArrowDiv.classList.add('arrowcontainer');
-            leftArrowDiv.appendChild(leftArrowImage);
-            leftArrowDiv.appendChild(previousDiv)
-            navigationElement[0].prepend(leftArrowDiv);
-
-            // Right Arrow
-            const rightArrowDiv = document.createElement('div');
-            const rightArrowImg = document.createElement('img');
-            nextDiv.textContent = 'Next';
-            rightArrowImg.src = './resources/Images/rightArrow.svg';
-            rightArrowImg.width = '16';
-            rightArrowImg.height = '16';
-            rightArrowDiv.classList.add('arrowcontainer');
-            rightArrowDiv.appendChild(nextDiv);
-            rightArrowDiv.appendChild(rightArrowImg);
-            navigationElement[0].appendChild(rightArrowDiv);
-        }
     };
     return { readyCallback, renderedCallback };
 };
@@ -151,14 +108,15 @@ window.__gcse.searchCallbacks = {
 };
 
 // Helper Function Section
-function stringLengthShortner(sentance, maxLength = 20) { // To convert long title to title...
+/* Function to convert long title to title... */
+function stringLengthShortner(sentance, maxLength = 20) {
     if (sentance.length > maxLength) {
         sentance = sentance.substring(0, maxLength) + '...';
     }
     return sentance;
 }
-
-function formatNumber(number) { // To convert them to K and M ie 1K or 1M
+/* Function to convert them to K and M ie 1K or 1M8 */
+function formatNumber(number) {
     if (number >= 1000000) {
         return (number / 1000000).toFixed(1) + "M";
     } else if (number >= 1000) {
@@ -167,6 +125,48 @@ function formatNumber(number) { // To convert them to K and M ie 1K or 1M
     return number.toString();
 }
 
+// Components
+function navBar() {
+    navigationElement = document.getElementsByClassName('gsc-cursor');
+    pageNumberComponent = document.getElementsByClassName('gsc-cursor-page');
+    const pageArray = Array.from(pageNumberComponent);
+    const currentPageIndex = Array.from(pageArray).findIndex(div => div.classList.contains('gsc-cursor-current-page'));
+
+    const previousDiv = pageArray[currentPageIndex - 1];
+    const currentDiv = pageArray[currentPageIndex];
+    const nextDiv = pageArray[currentPageIndex + 1];
+
+    previousDiv.classList.add('navBtn')
+    nextDiv.classList.add('navBtn')
+    currentDiv.classList.add('curr-navBtn')
+
+    // Custom Navigation Bar
+    if (navigationElement[0]) {
+        //Left Arrow            
+        const leftArrowDiv = document.createElement('div');
+        const leftArrowImage = document.createElement('img');
+        previousDiv.innerText = 'Prev'
+        leftArrowImage.src = './resources/Images/leftArrow.svg';
+        leftArrowImage.width = '16';
+        leftArrowImage.height = '16';
+        leftArrowDiv.classList.add('arrowcontainer');
+        leftArrowDiv.appendChild(leftArrowImage);
+        leftArrowDiv.appendChild(previousDiv)
+        navigationElement[0].prepend(leftArrowDiv);
+
+        // Right Arrow
+        const rightArrowDiv = document.createElement('div');
+        const rightArrowImg = document.createElement('img');
+        nextDiv.textContent = 'Next';
+        rightArrowImg.src = './resources/Images/rightArrow.svg';
+        rightArrowImg.width = '16';
+        rightArrowImg.height = '16';
+        rightArrowDiv.classList.add('arrowcontainer');
+        rightArrowDiv.appendChild(nextDiv);
+        rightArrowDiv.appendChild(rightArrowImg);
+        navigationElement[0].appendChild(rightArrowDiv);
+    }
+}
 function searchOnGoogleBtn() {
     const googleSearchDiv = document.getElementsByClassName('gcsc-find-more-on-google-query');
     if (googleSearchDiv) {
@@ -177,3 +177,11 @@ function searchOnGoogleBtn() {
     }
 }
 
+//Event Handlers
+const handleClick = (event) => {
+    // Access event properties and perform actions
+    console.log(event);
+    console.log('Event target:', event.target);
+
+    // ...other event-related operations
+};
