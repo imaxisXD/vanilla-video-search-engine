04cf0dbc9251fde6f436ff8323211f185b93617d
imaxisXD <sunny735084@gmail.com>
2023-05-18 18:29:05 +0530

    wip : thumnail styling

--- resources/script.js
+++ resources/script.js
@@ -33,13 +33,49 @@ const makeTwoPartCallback = () => {
             const div = results[i];
             const parent = div.parentNode;
             parent.classList.add('parent-container');
-
             const genre = musicCategoryVideoes[i]["genre"];
+
             if (genre === "Music") {
-                div.classList.add('result-container')
-                const innerDiv = document.createElement('div');
-                innerDiv.innerHTML = '<b>Views: ' + formatNumber(genre) + '</b>';
-                div.insertAdjacentElement('afterbegin', innerDiv);
+                div.innerHTML = '';
+                div.classList.add('result-card');
+
+                const imageDiv = document.createElement('div');
+
+                const imageElement = document.createElement('img');
+                imageElement.src = musicCategoryVideoes[i]["thumbnailImage"];
+                imageElement.width = '132';
+                imageElement.height = '74';
+                imageDiv.classList.add('thumnail')
+                imageDiv.appendChild(imageElement);
+                div.appendChild(imageDiv);
+
+                const infoDiv = document.createElement('div');
+                infoDiv.classList.add('result-card__right-container');
+                const urlAndViewDiv = document.createElement('div');
+
+                const childElementArray2 = [
+                    document.createElement('span'),
+                    document.createElement('span')
+                ];
+                childElementArray2[0].textContent = 'Youtube.com';
+                childElementArray2[1].textContent = musicCategoryVideoes[i].views;
+                for (const child of childElementArray2) {
+                    urlAndViewDiv.appendChild(child)
+                }
+                const childElementArray1 = [
+                    document.createElement('p'),
+                    document.createElement('p'),
+                    urlAndViewDiv
+                ];
+
+                childElementArray1[0].textContent = musicCategoryVideoes[i].title;
+                childElementArray1[1].textContent = musicCategoryVideoes[i].channel;
+
+                for (const child of childElementArray1) {
+                    infoDiv.appendChild(child);
+                }
+
+                div.appendChild(infoDiv);
             }
             else {
                 console.log(parent);
